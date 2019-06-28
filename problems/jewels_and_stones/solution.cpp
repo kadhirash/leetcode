cf26b84d6d875486a40d9588d4e173f67f133045
kadhirash <kadhirash@gmail.com>
2019-06-28 23:59:08 +0000

    Sync LeetCode submission - Jewels and Stones (cpp)

--- problems/jewels_and_stones/solution.cpp
+++ problems/jewels_and_stones/solution.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    int numJewelsInStones(string J, string S) {
-        //J = stones as jewels
-            //Distinct, case sensitive letters
-        //S = stones
-            //Each char. in s = stone
-        // How many S are J?
-        int count = 0;
-        for( int i = 0; i < S.length(); i ++){
-            for(int j = 0; j < J.length(); j++){
-                 if(J.at(j) == S.at(i)){
-                    count++;
-                }
-            }
-        }
-        return count;
+    int numJewelsInStones(string J, string S) 
+    {
+        int res = 0;
+        unordered_set<char> setJ(J.begin(), J.end());
+        for (char s : S) if (setJ.count(s)) res++;
+        return res;
     }
 };
