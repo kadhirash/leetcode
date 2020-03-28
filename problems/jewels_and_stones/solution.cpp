90fbd4412b5893c60461685c87ac589a16907c9c
kadhirash <kadhirash@gmail.com>
2020-03-28 03:10:19 +0000

    Sync LeetCode submission - Jewels and Stones (cpp)

--- problems/jewels_and_stones/solution.cpp
+++ problems/jewels_and_stones/solution.cpp
@@ -3,8 +3,8 @@ public:
     int numJewelsInStones(string J, string S) {
         int count = 0;
         for(int i = 0; i < S.length(); i ++){
-            for(int j = 0; j < J.length(); j++){
-                if(S.at(i) == J.at(j)){
+            for(int j = 0 ; j < J.length(); j++){
+                if(J.at(j) == S.at(i)){
                     count++;
                 }
             }
