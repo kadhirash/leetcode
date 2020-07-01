99707a2543cab966affad3ef6b4117c254dac12c
kadhirash <kadhirash@gmail.com>
2020-07-01 02:17:15 +0000

    Sync LeetCode submission - Jewels and Stones (python3)

--- problems/jewels_and_stones/solution.py
+++ problems/jewels_and_stones/solution.py
@@ -1,8 +1,6 @@
-class Solution(object):
-    def numJewelsInStones(self, J, S):
-        """
-        :type J: str
-        :type S: str
-        :rtype: int
-        """
-        return sum([i in J for i in S])
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        # return the sum  of num times of char s in J for s in S
+        #return sum(s in J for s in S) 
+        h = set(J)
+        return sum(s in h for s in S)
