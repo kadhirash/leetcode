1270b766adcf8880a950d82daa8793af9a857489
kadhirash <kadhirash@gmail.com>
2020-07-02 10:20:36 +0000

    Sync LeetCode submission - Defanging an IP Address (python3)

--- problems/defanging_an_ip_address/solution.py
+++ problems/defanging_an_ip_address/solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        return address.replace('.','[.]')
+        # naive: O(n) 
+        # ans = address.replace(".", "[.]")
+        # return ans
+        
+       # ans = {}
+    
+        return ''.join('[.]' if c == '.' else c for c in address)
+    
+        
+        
+        
