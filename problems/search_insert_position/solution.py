64b9a969465fbcd12a1be84d4839f57102ab8a4e
kadhirash <kadhirash@gmail.com>
2020-07-02 03:36:39 +0000

    Sync LeetCode submission - Search Insert Position (python3)

--- problems/search_insert_position/solution.py
+++ problems/search_insert_position/solution.py
@@ -1,17 +1,29 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            ans = (left + right) // 2
-            if nums[ans] == target:
-                return ans
-            if target < nums[ans]:
-                right = ans - 1
-            else:
-                left = ans + 1
-        return left
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+#         for counter, value in enumerate(nums):
+#             #print(counter,value)
+#             if value == target:
+#                 return counter
+#             else:
+                
+    
+        low = 0
+        high = len(nums) - 1
+        mid = 0
+        while low <= high:
+            mid = (low + high) // 2
+            # Check if x is present at mid 
+            if nums[mid] < target: 
+                low = mid + 1
+  
+            # If x is greater, ignore left half 
+            elif nums[mid] > target: 
+                high = mid - 1
+
+            # If x is smaller, ignore right half 
+            else: 
+                return mid 
+  
+        # If we reach here, then the element was not present 
+        return low
+        
