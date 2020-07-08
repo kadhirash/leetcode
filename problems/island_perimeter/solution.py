f6bf6eabedaee4f64b9ecf617a6e090a387fb083
kadhirash <kadhirash@gmail.com>
2020-07-08 03:55:46 +0000

    Sync LeetCode submission - Island Perimeter (python3)

---
+++ problems/island_perimeter/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+        # if grid == None or len(grid) == 0:
+        #     return 0
+                                                                      
+        for row in range(len(grid)):                                  
+            for col in range(len(grid[row])): 
+                if grid[row][col] == 1:                              
+                    perimeter += 4
+                    
+                    
+                    if col > 0 and grid[row][col-1] == 1:
+                        perimeter -= 2
+                        
+                    if row > 0 and grid[row-1][col] == 1:
+                        perimeter -= 2
+                        
+                
+        return perimeter
