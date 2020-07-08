e39fad31f8ada8c295188d607a354a4ac4ace2df
kadhirash <kadhirash@gmail.com>
2020-07-08 18:52:45 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashl = {}
+        # return indices of 2 numbers so it adds up to target
+        # only 1 solution
         
-        for i, iterate in enumerate(nums):
-            if iterate in hashl:
-                return [hashl[iterate], i]
-            else:
-                hashl[target-iterate] = i
+        
+        # for loop to iterate through the array
+        # 2nd for loop to keep track of indices
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                #print(i,j)
+                if nums[i] + nums[j] == target and i!= j:
+                    return i,j
+                    
