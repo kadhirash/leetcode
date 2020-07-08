442b01384876c3d6c5a38d7984a7433c85f15752
kadhirash <kadhirash@gmail.com>
2020-07-08 14:07:13 +0000

    Sync LeetCode submission - Hamming Distance (python3)

--- problems/hamming_distance/solution.py
+++ problems/hamming_distance/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         xor = x^y
-        dist = 0
+        distance = 0
         while xor:
-            dist += 1
-            xor = xor & (xor - 1)
-        print(dist)
-        return dist
+            if xor & 1:
+                distance += 1
+            xor = xor >> 1
+        return distance
