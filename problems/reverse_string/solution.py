9d1610aad743f2c3ce6147ecde80abc2b0912b5f
kadhirash <kadhirash@gmail.com>
2020-07-13 01:28:55 +0000

    Sync LeetCode submission - Reverse String (python3)

--- problems/reverse_string/solution.py
+++ problems/reverse_string/solution.py
@@ -3,6 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
-            
-            
+        def helper(left, right):
+            if left <= right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+                
+        helper(0, len(s) -1)
+        
