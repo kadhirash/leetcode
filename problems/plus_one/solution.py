6f5cbd7d57316f3b1f9c5f269048f0eeb77e3884
kadhirash <kadhirash@gmail.com>
2020-07-13 18:27:34 +0000

    Sync LeetCode submission - Plus One (python3)

--- problems/plus_one/solution.py
+++ problems/plus_one/solution.py
@@ -1,26 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
-        # no need to check for non-empty
-        
-        #increase one to integer
-        # sig. digit is at head of list, and no leading 0 except 0 itself
-        
-        # return arry with incrementation
+        # Plan 
+        # for loop through the array 
+        # if 9 at the end --> 0 
+        # increment by one 
+        # return new array
         
         
-        # Plan
-        
-        # Loop through array
-        # if 9 at the end, change to a 0 else change last digit to + 1
-        # 
         for i in range(len(digits)):
-            index = len(digits)-1-i
-            #print(index)
-            if digits[index] == 9:
-                digits[index] = 0
+            n = len(digits)-1-i # second to last digit
+            
+            if digits[n] == 9:
+                digits[n] = 0
             else:
-                digits[index] += 1
-                print(digits)
+                digits[n] += 1
                 return digits
+        
         return [1] + digits
