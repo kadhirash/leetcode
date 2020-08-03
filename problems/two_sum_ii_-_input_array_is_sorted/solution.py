e7313e879e1be7ac91246b4968cb5536b95a3fb5
kadhirash <kadhirash@gmail.com>
2020-08-03 03:29:58 +0000

    Sync LeetCode submission - Two Sum II - Input array is sorted (python3)

--- problems/two_sum_ii_-_input_array_is_sorted/solution.py
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.py
@@ -1,15 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        front = 0
-        back = len(numbers)-1
+        front,back = 0, len(numbers)-1
         
-        while front < back:
-            sum_ans = numbers[front] + numbers[back] 
-            
-            if sum_ans == target:
-                return (front+1, back + 1)
-            
-            if sum_ans < target:
+        while front <= back:
+            num = numbers[front] + numbers[back]
+            if num == target:
+                return (front + 1, back + 1)
+            elif num < target:
                 front += 1
             else:
                 back -= 1
+        return [-1,-1]
