1594884d3464e7a9f8b53a90e1db925832ea016d
kadhirash <kadhirash@gmail.com>
2020-08-03 03:15:07 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,20 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-       
-    ##TLE
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 if nums[i] + nums[j] == target and i!= j:
-#                     return i,j
+        # for i in range(len(nums)):
+        #     for j in range(i, len(nums)):
+        #         if nums[i] + nums[j] == target and i!=j:
+        #             return [i,j]
         
+        # ans = { }
+        # for i, val in enumerate(nums):
+        #     remains = target - val
+        #     if remains in ans:
+        #         return [ans[remains], i]
+        #     ans[val] = i
         
-        
-        hash_map = {}
-        for i, x in enumerate(nums):
-           
-            if x in hash_map:
-                return [hash_map[x],i]
+        ans = {}
+        for i in range(len(nums)):
+            remain = target-nums[i]
+            if remain not in ans:
+                ans[nums[i]] = i
             else:
-                hash_map[target - x] = i
-              
+                return [ans[remain], i]
         
