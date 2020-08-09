e138cb7dbf677179f5ddad404c5a6527a200cca6
kadhirash <kadhirash@gmail.com>
2020-08-09 11:08:09 +0000

    Sync LeetCode submission - Plus One (python3)

--- problems/plus_one/solution.py
+++ problems/plus_one/solution.py
@@ -1,19 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # Plan 
-        # for loop through the array 
-        # if 9 at the end --> 0 
-        # increment by one 
-        # return new array
+        # increase one to integer
+        # MSD -> head of list
         
-        
-        for i in range(len(digits)):
-            n = len(digits)-1-i # second to last digit
-            
-            if digits[n] == 9:
-                digits[n] = 0
-            else:
-                digits[n] += 1
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] < 9:
+                digits[i] += 1
                 return digits
-        
-        return [1] + digits
+            digits[i] = 0
+            
+        res =  [0] * (len(digits) + 1)
+        res[0] = 1
+        return res
