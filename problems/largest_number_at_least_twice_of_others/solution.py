8e646a4963d272468a71630447f3ea36f98e4ccf
kadhirash <kadhirash@gmail.com>
2020-08-09 11:00:26 +0000

    Sync LeetCode submission - Largest Number At Least Twice of Others (python3)

--- problems/largest_number_at_least_twice_of_others/solution.py
+++ problems/largest_number_at_least_twice_of_others/solution.py
@@ -1,14 +1,28 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
-        # sort the array of nums
-        # find the largest (end-1), 2nd largest (end -2)
-        # check if largest >= 2 * 2nd largest
-            # if true, return i 
-            # else return -1
-        # if length 1, return 0
-            
+        # find largest number using max
         max_num = max(nums)
-        if all(max_num >= 2*x for x in nums if x != max_num):
+        #iterate through nums and check if max_num is 2*nums[i]
+        
+        if(len(nums)) == 1:
             return nums.index(max_num)
-        return -1
-                
+        for i in range(len(nums)):
+            max_num_i = nums.index(max_num)
+        for x in range(len(nums)):
+            if x != max_num_i and nums[max_num_i] < 2 * nums[x]:
+                return -1
+        return max_num_i
+    
+    
+    
+    #  int maxIndex = 0;
+    #     for (int i = 0; i < nums.length; ++i) {
+    #         if (nums[i] > nums[maxIndex])
+    #             maxIndex = i;
+    #     }
+    #     for (int i = 0; i < nums.length; ++i) {
+    #         if (maxIndex != i && nums[maxIndex] < 2 * nums[i])
+    #             return -1;
+    #     }
+    #     return maxIndex;
+    # }
