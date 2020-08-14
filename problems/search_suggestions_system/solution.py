5946e3c9759d99002808a429ea77b7051ca92b97
kadhirash <kadhirash@gmail.com>
2020-08-14 03:20:11 +0000

    Sync LeetCode submission - Search Suggestions System (python3)

--- problems/search_suggestions_system/solution.py
+++ problems/search_suggestions_system/solution.py
@@ -1,26 +1,27 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        class TrieNode:
-            def __init__(self):
-                self.children = defaultdict(TrieNode)
-                self.words = []
-            def insert(self, word):
-                if len(self.words) < 3:
-                    self.words.append(word)
-                    
-                    
-        products = sorted(products)
-        root = TrieNode()
-        
-        for i in products:
-            curr = root
-            for c in i:
-                curr = curr.children[c]
-                curr.insert(i)
-                
-        res,curr = [], root
-        for c in searchWord:
-            curr = curr.children[c]
-            res.append(curr.words)
-        return res
+        products.sort() # time O(nlogn)
+        array_len = len(products)
+        ans = []
+        input_char = ""
+
+        for chr in searchWord:
+            tmp = []
+            input_char += chr
+            insertion_index = self.binary_search(products, input_char) # find where input_char can be inserted in-order in the products array
+            for word_ind in range(insertion_index, min(array_len, insertion_index+3)): # check the following 3 words, if valid
+                if products[word_ind].startswith(input_char):
+                    tmp.append(products[word_ind])
+            ans.append(tmp)
+        return ans
+
+    def binary_search(self, array, target): # bisect.bisect_left implementation
+        lo = 0
+        hi = len(array)
+
+        while lo < hi:
+            mid = (lo + hi) //2
+            if array[mid] < target: lo = mid + 1
+            else: hi = mid
+        return lo
         
