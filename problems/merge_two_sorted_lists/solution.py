43e4cadc79686f455341a95fcc62a2a16e1ad466
kadhirash <kadhirash@gmail.com>
2020-08-14 14:07:44 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -5,41 +5,29 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-#         # recursive
-        
-#         # edge cases
+        #recursive
 #         if not l1: return l2
 #         if not l2: return l1
-#         if not l1 or not l2: return None
+#         if not l1 and not l2: return None
         
-#         if l1.val < l2.val:
-#             l1.next = self.mergeTwoLists(l1.next, l2)
-#             return l1
-#         else:
-#             l2.next = self.mergeTwoLists(l1,l2.next)
-#             return l2
-
+#         while l1 and l2:
+#             if l1.val <= l2.val:
+#                 l1.next = self.mergeTwoLists(l1.next, l2)
+#                 return l1
+#             else:
+#                 l2.next = self.mergeTwoLists(l1, l2.next)
+#                 return l2
         #iterative
-        
-        dummy = ListNode(-1)
-        head = dummy
+        l3 = ListNode(-1)
+        head = l3
         
         while l1 and l2:
-            if l1.val < l2.val:
+            if l1.val <= l2.val:
                 head.next = l1
                 l1 = l1.next
             else:
                 head.next = l2
                 l2 = l2.next
             head = head.next
-        
-        if l1:
-            head.next = l1
-            l1 = l1.next
-        if l2:
-            head.next = l2
-            l2 = l2.next
-        
-        return dummy.next
-            
-        
+        head.next = l1 or l2
+        return l3.next
