03e48e9f97958b3f6ccc3692f2fbf03305e00dad
kadhirash <kadhirash@gmail.com>
2020-08-17 04:07:59 +0000

    Sync LeetCode submission - Prison Cells After N Days (python3)

--- problems/prison_cells_after_n_days/solution.py
+++ problems/prison_cells_after_n_days/solution.py
@@ -1,17 +1,18 @@
 class Solution:
+    def next_step(self, cells):
+        res = [0] * 8
+        for i in range(1,7):
+            res[i] = int(cells[i-1] == cells[i+1])
+        return res
     def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
-        
-        if N % 14 == 0:
-            N = 14
-        else:
-            N = N % 14
-            
-        for i in range(1, N + 1):
-            temp = [0] * len(cells)
-            for i in range(1, len(cells)-1):
-                if cells[i-1] == cells[i+1]:
-                    temp[i] = 1
-                else:
-                    temp[i] = 0
-            cells = temp
+        found_dic = {}
+        for i in range(N):
+            cells_str = str(cells)
+            if cells_str in found_dic:
+                loop_len = i - found_dic[cells_str]
+                return self.prisonAfterNDays(cells, (N - i) % loop_len)
+            else:
+                found_dic[cells_str] = i 
+                cells = self.next_step(cells) 
+                
         return cells
