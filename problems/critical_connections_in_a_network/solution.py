4a2851b92ef494b1ce08a50929da4a0802ce26e7
kadhirash <kadhirash@gmail.com>
2020-08-18 02:41:59 +0000

    Sync LeetCode submission - Critical Connections in a Network (python3)

--- problems/critical_connections_in_a_network/solution.py
+++ problems/critical_connections_in_a_network/solution.py
@@ -1,32 +1,24 @@
-import collections
-class Solution(object):
-    def criticalConnections(self, n, connections):
-        def makeGraph(connections):
-            graph = collections.defaultdict(list)
-            for conn in connections:
-                graph[conn[0]].append(conn[1])
-                graph[conn[1]].append(conn[0])
-            return graph
-
-        graph = makeGraph(connections)
-        connections = set(map(tuple, (map(sorted, connections))))
-        rank = [-2] * n
-
-        def dfs(node, depth):
-            if rank[node] >= 0:
-                # visiting (0<=rank<n), or visited (rank=n)
-                return rank[node]
-            rank[node] = depth
-            min_back_depth = n
-            for neighbor in graph[node]:
-                if rank[neighbor] == depth - 1:
-                    continue  # don't immmediately go back to parent. that's why i didn't choose -1 as the special value, in case depth==0.
-                back_depth = dfs(neighbor, depth + 1)
-                if back_depth <= depth:
-                    connections.discard(tuple(sorted((node, neighbor))))
-                min_back_depth = min(min_back_depth, back_depth)
-            rank[node] = n  # this line is not necessary. see the "brain teaser" section below
-            return min_back_depth
-            
-        dfs(0, 0)  # since this is a connected graph, we don't have to loop over all nodes.
-        return list(connections)
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        def dfs(previous = -1, current = 0, depth = 1):
+            nonlocal depths, output
+            temp_depth = depth
+            depths[current] = depth
+            for neighbor in graph[current]:
+                if neighbor == previous:
+                    continue
+                neighbor_depth = depths[neighbor] or dfs(current, neighbor, depth + 1)
+                if depth < neighbor_depth:
+                    output.append((current, neighbor))
+                elif neighbor_depth < temp_depth:
+                    temp_depth = neighbor_depth
+            depths[current] = temp_depth
+            return temp_depth
+        graph = [[] for _ in range(n)]
+        depths = [0] * n
+        output = []
+        for u, v in connections:
+            graph[u].append(v)
+            graph[v].append(u)
+        dfs()
+        return output
