f41a0cb3488656a56c79f21f9c6b94412af5959d
kadhirash <kadhirash@gmail.com>
2020-08-18 01:05:36 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,21 +1,32 @@
 class Solution:
+    def is_valid(self, grid, r, c):
+        m, n = len(grid), len(grid[0])
+        if r < 0 or c < 0 or r >= m or c >= n:
+            return False
+        return True
+    
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
+        if not grid or not grid[0]:
             return 0
+
+        m, n = len(grid), len(grid[0])
         count = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == '1':
-                    self.dfs(grid, row, col)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.bfs(grid, i, j)
                     count += 1
         return count
-    
-    
-    def dfs(self, grid, row, col):
-        if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or grid[row][col] != '1':
-            return
-        grid[row][col] = '#'
-        self.dfs(grid, row+1, col)
-        self.dfs(grid, row-1, col)
-        self.dfs(grid, row, col+1)
-        self.dfs(grid, row, col-1)
+
+    def bfs(self, grid, r, c):
+        queue = collections.deque()
+        queue.append((r, c))
+        grid[r][c] = '0'
+        while queue:
+            directions = [(0,1), (0,-1), (-1,0), (1,0)]
+            r, c = queue.popleft()
+            for d in directions:
+                nr, nc = r + d[0], c + d[1]    
+                if self.is_valid(grid, nr, nc) and grid[nr][nc] == '1':
+                    queue.append((nr, nc))
+                    grid[nr][nc] = '0'
