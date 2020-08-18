f10bbe43265835e93e593e99afe0ce08857ed6c0
kadhirash <kadhirash@gmail.com>
2020-08-18 20:06:37 +0000

    Sync LeetCode submission - Search a 2D Matrix II (python3)

--- problems/search_a_2d_matrix_ii/solution.py
+++ problems/search_a_2d_matrix_ii/solution.py
@@ -5,13 +5,16 @@ class Solution:
         :type target: int
         :rtype: bool
         """
-        if not matrix: return False
+        if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
 
-        row, col = 0, len(matrix[0])-1
-        
+        row, col = 0, len(matrix[0]) - 1
+
         while row < len(matrix) and col >= 0:
-            if matrix[row][col] == target: return True
-            elif matrix[row][col] < target: row +=1
-            else: col -=1 
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col -= 1
+            else:
+                row += 1
         return False
-       
