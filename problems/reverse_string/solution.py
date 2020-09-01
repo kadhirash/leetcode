9df634ff7b79a053723eb395696871a513e5dcfe
kadhirash <kadhirash@gmail.com>
2020-09-01 10:47:36 +0000

    Sync LeetCode submission - Reverse String (python3)

--- problems/reverse_string/solution.py
+++ problems/reverse_string/solution.py
@@ -3,15 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        # 2 pointers 
-        # 1 pointer starts at the front of the string
-        # 1 pointer starts at the back of the string 
-        # increment the front, decrement the back
-        
-        front = 0
-        back = len(s)-1 
-        
-        while front < back:
-            s[front],s[back] = s[back], s[front]
-            back -= 1
-            front += 1
+        left,right = 0, len(s)-1
+        while left < right:
+            s[left],s[right] = s[right],s[left]
+            left += 1
+            right -= 1
+    
