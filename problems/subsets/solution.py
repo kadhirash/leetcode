f4270cfa1f67c1186cdaff985d8daa973d6b8bd6
kadhirash <kadhirash@gmail.com>
2020-09-02 23:04:19 +0000

    Sync LeetCode submission - Subsets (python3)

--- problems/subsets/solution.py
+++ problems/subsets/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
+        ans = [ [] ]
         for ints in nums:
-            ans += [curr_int + [ints] for curr_int in ans]
+            for curr_int in ans:
+                ans = ans + [curr_int + [ints]]
         return ans
