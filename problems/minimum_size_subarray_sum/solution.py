0808699a58f5f14cc85827a967909b6acd5a4bf4
kadhirash <kadhirash@gmail.com>
2020-09-05 00:41:17 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (python3)

--- problems/minimum_size_subarray_sum/solution.py
+++ problems/minimum_size_subarray_sum/solution.py
@@ -1,28 +1,13 @@
 class Solution:
     def minSubArrayLen(self, s: int, nums: List[int]) -> int:
-        if len(nums) == 0 or nums == []:
-            return 0
-        
-        #two pointers
-        # i => fast ptr...thinks greedily..moves l to r and increases window size, thereby adding local sum
-        # j => slow ptr...pulls back the window size, thereby satisfying the min_size_subarr_len objective (in simple terms it eliminates the elements from start of array)
-        
-        
-        i, j, sum, local_len, min_len, length = 0, 0, 0, 0, float('inf'), len(nums)
-        while i < length:
-
-            #greedily increase window
-            sum += nums[i]
-            local_len += 1
-
-            #pulling back window size
-            while sum >= s:
-                if local_len < min_len:
-                    min_len = local_len
-                local_len -= 1
-                sum -= nums[j]
-                j += 1
-            i += 1
-        if min_len == float('inf'): 
-            min_len = 0
+        if not s or not nums: return 0
+        min_len = float('inf')
+        windowStart, windowSum = 0,0
+        for windowEnd in range(len(nums)):
+            windowSum += nums[windowEnd]
+            while windowSum >= s:
+                min_len = min(min_len, windowEnd-windowStart + 1)
+                windowSum -= nums[windowStart]
+                windowStart += 1
+        if min_len == float('inf'): return 0
         return min_len
