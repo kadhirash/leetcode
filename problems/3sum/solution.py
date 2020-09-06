b4775ae377807f0425038e6e61048335c16d50f7
kadhirash <kadhirash@gmail.com>
2020-09-06 03:56:34 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,30 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        #store ans in a list
-        ans = [ ] 
-        # sort numbers from least to greatest
-        nums.sort()
-        
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums [i-1]:
-                continue #to eliminate duplicate values
-            left = i + 1
-            right = len(nums) - 1
-            
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total < 0: #-4, -1, -1, 0, 1, 2
-                    left = left + 1
-                elif total > 0:
-                    right = right - 1
-                else:
-                    ans.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]: # make sure left doesn't repeat
-                        left = left + 1
-                    while left <=  right and nums[right] == nums[right - 1]: # make sure right doesn't repeat
-                        right = right - 1 
-                    #after making sure no duplicate left or right values
-                    left = left + 1
-                    right = right - 1
-        return ans
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return res
