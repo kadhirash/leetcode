568d47c78ed12c82a41c3d0bed3e6082708eef1e
kadhirash <kadhirash@gmail.com>
2020-09-06 20:16:12 +0000

    Sync LeetCode submission - Word Search (python3)

--- problems/word_search/solution.py
+++ problems/word_search/solution.py
@@ -1,20 +1,43 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # find if word exists
+            # word formed by adjacent horizontal/vertical neighboring cells
+            # same cell can't be used more than once
+        
+        
+        # 2 x 2 
+        # only lower/upper case letters
+        
+        if not board: return False
+        
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if self.dfs(board,row,col,word,0):
                     return True
         return False
     
-    def dfs(self,board,row,col,word,i):
-        if i == len(word): return True
-        if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]) or board[row][col] != word[i]: return False
+    
+    def dfs(self,board,row,col,word,char):
+        
+        # validation checks
+        if char == len(word): return True
+        
+        if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]) or board[row][col] != word[char]: return False
+        
+        
+        # curr state
         
         state = board[row][col]
         
-        board[row][col] = ''
+        # mark visited
+        board[row][col] = '#'
         
-        ans = self.dfs(board,row+1,col,word,i+1) or self.dfs(board,row-1,col,word,i+1) or self.dfs(board,row,col+1,word,i+1) or self.dfs(board,row,col-1,word,i+1)
+        ans = self.dfs(board,row-1,col,word,char+1) or \
+        self.dfs(board,row+1,col,word,char+1)or \
+        self.dfs(board,row,col-1,word,char+1)or \
+        self.dfs(board,row,col+1,word,char+1)
+        
+        # revert state
         
         board[row][col] = state
         
