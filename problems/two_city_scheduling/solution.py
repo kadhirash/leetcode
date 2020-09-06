7127dfd6dcd3c02938b763384302726e5abebe30
kadhirash <kadhirash@gmail.com>
2020-09-06 22:58:12 +0000

    Sync LeetCode submission - Two City Scheduling (python3)

--- problems/two_city_scheduling/solution.py
+++ problems/two_city_scheduling/solution.py
@@ -1,13 +1,27 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        first_city= []
-        for city_one,city_two in costs:
-            first_city.append(city_one)
-        diff = []
-        for city_one, city_two in costs:
-            diff.append(city_two-city_one)
-            
-        refund = diff.sort()
-        refund = diff[:len(costs)//2]
+        # 2n people
+        # acosti = citya, bcosti = city b
+        # return min. cost to fly every person, so citya = cityb 
         
-        return sum (first_city + refund)
+        
+        # 2n == len(costs)
+        
+        # each city = [i,j]
+        # get city[i] of city a values
+        # find differences between city[j-i] 
+        
+            # Since some neg., sort them and then return the smallest values
+        # return sum of city a + smallest diff value
+        
+        
+        city_a = []
+        for a,b in costs:
+            city_a.append(a)
+        diff = []
+        for a,b in costs:
+            diff.append(b-a)
+        
+        small = diff.sort()
+        small = diff[:len(costs)//2]
+        return sum(city_a + small)
