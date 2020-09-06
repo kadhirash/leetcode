3ef6a9986a3306f83005e668d43bfcae85c221af
kadhirash <kadhirash@gmail.com>
2020-09-06 22:39:02 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- problems/valid_anagram/solution.py
+++ problems/valid_anagram/solution.py
@@ -1,3 +1,29 @@
+import collections
+from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        # same length, only lowercase alphabets
+        # consist of the same amount of characters
+        
+        # 3 t's, 4 d's, 5 c's 
+        # s= tttddddccccc.     t= tcdtcdtcddcc
+        
+        # use hashmap to keep track of values and indexes
+        # iterate through string s 
+            # increment s values, decrement t values and compare
+        
+        # iterate through hashmap
+            # if letters in hashmap !0, return False else true
+        
+        if len(s) != len(t): return False
+        
+        anagram = defaultdict(int)
+        
+        for letters in range(len(s)):
+            anagram[s[letters]] += 1
+            anagram[t[letters]] -= 1
+            
+        for letters in anagram:
+            if anagram[letters] != 0:
+                return False
+        return True
