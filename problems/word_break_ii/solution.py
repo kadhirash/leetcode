0752e80b5cebd63c456db9ca11dfad7ea94a3ec8
kadhirash <kadhirash@gmail.com>
2020-09-08 20:24:44 +0000

    Sync LeetCode submission - Word Break II (python3)

--- problems/word_break_ii/solution.py
+++ problems/word_break_ii/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        memo = {len(s): ['']}
-        def sentences(i):
-            if i not in memo:
-                memo[i] = [s[i:j] + (tail and ' ' + tail)
-                           for j in range(i+1, len(s)+1)
-                           if s[i:j] in wordDict
-                           for tail in sentences(j)]
-            return memo[i]
-        return sentences(0)
+        wordSet =set(wordDict)
+        @functools.lru_cache
+        def dfs(start: int):
+            if start == len(s):
+                return [[]]
+            res = []
+            for i in range(start, len(s)):
+                if s[start:i+1] in wordSet:
+                    res += [[s[start:i+1]] + child for child in dfs(i+1)]
+            return res
+        return [" ".join(line) for line in dfs(0)]
     
