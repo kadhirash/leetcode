49a0ab4fba7bc384d51a2c32a101af6d0be07aeb
kadhirash <kadhirash@gmail.com>
2020-09-08 00:21:29 +0000

    Sync LeetCode submission - Meeting Rooms II (python3)

--- problems/meeting_rooms_ii/solution.py
+++ problems/meeting_rooms_ii/solution.py
@@ -1,13 +1,44 @@
+import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # given start and end times, find min. number of conference rooms
+        
+        # [[0,30], [15,20]], [5,10]
+        
+        # sort by interval start time
+        # [[0,30], [5,10], [15,20]]
+        
+        # use minheap
+            # push in values of the prev. end time and compare with curr start time
+            # if prev end time > curr start time, push in curr_end time, else pop off 
+                # [0,30] --> 30
+                # since 30 > 5, i can push in 10
+                
+                # [10,30]
+                    # 10 < 15 !
+                # [20, 30]
+                # return len of heap = # of rooms
+                
+                
         if not intervals: return 0
-        intervals.sort(key=lambda x:x[0]) # Sort by start times for intervals
-        heap = [intervals[0][1]]
-        for meeting in intervals[1:]:
-            # check if earliest meeting ends in room will finish before curr. meeting
-            # remove earliest meeting
-            if heap[0] <= meeting[0]:
-                heapq.heappop(heap)
-            heapq.heappush(heap, meeting[1])
-            # add curr meeting
-        return len(heap)
+        
+        
+        intervals.sort(key=lambda x:x[0])
+        
+        rooms = [] # minheap
+        
+        heapq.heappush(rooms,intervals[0][1])
+        
+        for i in range(1,len(intervals)):
+            if rooms[0] <= intervals[i][0]:
+                heapq.heappop(rooms)
+
+            heapq.heappush(rooms,intervals[i][1])
+        return len(rooms)
+            
+        
+        
+                
+                
+                
+                
