6c3b79a1aa84fc85e5e5aa34829af2ba3f1d63cc
kadhirash <kadhirash@gmail.com>
2020-09-13 06:39:08 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates In String (python3)

--- problems/remove_all_adjacent_duplicates_in_string/solution.py
+++ problems/remove_all_adjacent_duplicates_in_string/solution.py
@@ -1,11 +1,27 @@
 class Solution:
     def removeDuplicates(self, S: str) -> str:
-        ans = []
-        for chars in S:
-            if ans and ans[-1] == chars: # if ans and chars is in the top of the ans stack
-                ans.pop() # pop off char
-            else:
-                ans.append(chars) # append char
-        return ''.join(ans)
-    
-    
+        # remove the 2 adjacent and equal letters until no more duplicates
+        
+        # Clarification:
+            # S = lowercase letters
+        
+        # Example:
+            # abbaca
+                # aaca
+                # ca
+        # WalkThrough:
+            # create a stack to hold answer -> O(n-d) space
+            # in stack, add string chars
+                # if the last string char on stack == next string char, then pop 
+            # repeating until no more can be removed
+            # return as a string form
+            
+        
+        i,n = 0,len(S)
+        res = list(S)
+        for j in range(n):
+            res[i]=res[j]
+            if i>0 and res[i-1]==res[j]:
+                i-=2
+            i+=1
+        return ''.join(res[:i])
