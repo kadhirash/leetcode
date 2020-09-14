5b430c0a6040add24cf3c49f2bb3daf9a49244ef
kadhirash <kadhirash@gmail.com>
2020-09-14 16:15:44 +0000

    Sync LeetCode submission - Two City Scheduling (python3)

--- problems/two_city_scheduling/solution.py
+++ problems/two_city_scheduling/solution.py
@@ -1,27 +1,33 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # 2n people
-        # acosti = citya, bcosti = city b
-        # return min. cost to fly every person, so citya = cityb 
+        # Clarifications:
+            # len(costs) = even
+            # 2n people -> n people in A, n people in B
+        # Question to Solve:
+            # Return min. cost to fly every person to city so n people arrive from a and b
+        # Example:
+            # costs = [[10,20],[30,200],[400,50],[30,20]]
+            # 110
+            
+        # Strategy:
+            # Send everyone from city A 
+                # 10 30 400 30 -> 470
+            # Find differences between City B and city A
+                # 10 170 -350 -10
+            # Sort by the differences (from least to greatest)
+                # -350 -10 10 170
+            # Return sum of City A values + 1/2 len of differences costs
+            
+        min_cost = 0
+        n = len(costs) // 2
+        differences = []
+        for city_a,city_b in costs:
+            min_cost += city_a
+            differences.append(city_b-city_a)
         
+        differences.sort()
         
-        # 2n == len(costs)
-        
-        # each city = [i,j]
-        # get city[i] of city a values
-        # find differences between city[j-i] 
-        
-            # Since some neg., sort them and then return the smallest values
-        # return sum of city a + smallest diff value
-        
-        
-        city_a = []
-        for a,b in costs:
-            city_a.append(a)
-        diff = []
-        for a,b in costs:
-            diff.append(b-a)
-        
-        small = diff.sort()
-        small = diff[:len(costs)//2]
-        return sum(city_a + small)
+        for refund in range(n):
+            min_cost += differences[refund]
+            
+        return min_cost
