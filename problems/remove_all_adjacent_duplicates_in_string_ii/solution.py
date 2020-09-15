2064abf1d9dae27e516614a99e4c417ac5fd22e1
kadhirash <kadhirash@gmail.com>
2020-09-15 00:11:43 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (python3)

--- problems/remove_all_adjacent_duplicates_in_string_ii/solution.py
+++ problems/remove_all_adjacent_duplicates_in_string_ii/solution.py
@@ -1,49 +1,38 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        # choosing 'k' adjacent and equal letters from 's'
-            # removing them so left and right side of deleted strings get conctenated
-        # repeatedly make 'k'duplicate removals on 's' until we can't
+        # Clarifications:
+            # the answer is unique
+            # s only consists of lower case english letters
+        # To Do:
+            # return s w/o duplicates
+        # Example:
+            # deeedbbcccbdaa, k = 3     # remove e
+                # ddbbcccbdaa           # remove c
+                # ddbbbdaa              # remove b
+                # dddaa                 # remove d
+                # aa
         
-        # return final 's'
+        # Strategy:
+            # Use a stack to hold string
+                # Have a tuple in the stack (char, counter)
+            # iterate through the string
+                # when comparing the [-1][0] increment counter
+                    # if [-1][1] == k, then pop off k amount
+            # else append 
+            # make stack -> string and return
+                # .join in python and a for loop 
         
-    # Clarifications:
-        # s is all lowercase 
-        # 2 - k - 10^4 
-
-    # Examples:
-        # 'abcd', 2
-            # 'abcd'
-        # 'deeedbbcccbdaa', 3
-            # 'ddbbbdaa' -> removed eee, ccc
-            # 'dddaa' -> removed bbb
-            # 'aa' -> removed ddd
-    
-    # Walkthrough
-    # keep track of length of string
-    # Iterate through the string, w/ count variable
-        # If the curr char same as the one after it, then increase the count else reset to 1 
-        # if count == k then remove the last k chars
-    # repeat until length of string can't be changedd 
-    
-        i = 0
-        while i < len(s):
-            if s[i:i+k] == s[i] * k:
-                s = s[:i] + s[i+k:]
-                i = 0
+        
+        ans = [['#',0]]
+        for char in s:
+            if ans[-1][0] == char:
+                ans[-1][1] += 1
+                if ans[-1][1] == k:
+                    ans.pop()
             else:
-                i += 1
-        return s
-    
-     # i=0
-     #    while i<len(s):
-     #        if s[i:i+k]==s[i]*k:
-     #            s=s[:i]+s[i+k:]
-     #            i=0
-     #        else:
-     #            i+=1
-     #    return s
-
+                ans.append([char,1])
         
-    
-        
-    
+        string = ''
+        for i in range(len(ans)):
+            string += ans[i][0] * ans[i][1]
+        return string
