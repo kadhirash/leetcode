29da7cfaf9be2fe4d698229527afde4768f17289
kadhirash <kadhirash@gmail.com>
2020-09-15 04:02:22 +0000

    Sync LeetCode submission - Flatten a Multilevel Doubly Linked List (python3)

--- problems/flatten_a_multilevel_doubly_linked_list/solution.py
+++ problems/flatten_a_multilevel_doubly_linked_list/solution.py
@@ -7,22 +7,28 @@ class Node:
         self.next = next
         self.child = child
 """
+'''
+stack = [--4---5---6--NULL]
 
+
+1---2---3- 7---8-11--12--NULL--9---10--NULL
+'''
 class Solution:
     def flatten(self, head: 'Node') -> 'Node':
         if not head:
             return head
-        stack, prev = [head], Node(0)
-        while stack:
-            root = stack.pop()
-            root.prev = prev
-            prev.next = root
-            prev = root
-            if root.next:
-                stack.append(root.next)
-            if root.child:
-                stack.append(root.child)
-                root.child = None
-        head.prev = None
-        return head
+        temp = head
+        stack = []
+        while head:
+            if head.child:
+                if head.next:
+                    stack.append(head.next)
+                head.next = head.child
+                head.next.prev = head
+                head.child = None
+            elif not head.next and stack:
+                head.next = stack.pop()
+                head.next.prev = head
+            head = head.next
+        return temp
         
