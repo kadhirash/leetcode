0040bc76c1cdfbe9ae9ed6c42291843700730b27
kadhirash <kadhirash@gmail.com>
2020-09-17 03:52:35 +0000

    Sync LeetCode submission - Top K Frequent Words (python3)

--- problems/top_k_frequent_words/solution.py
+++ problems/top_k_frequent_words/solution.py
@@ -1,6 +1,22 @@
+import collections
+import heapq
+from heapq import heapify, heappush, heappop
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+        # count freq. of word and add to heap to store best 'k' candidates
+            # worst candidates at top of heap, then pop off 'k' times and reverse
+            # so best candidates are there
         count = collections.Counter(words)
-        heap = [(-freq, word) for word, freq in count.items()]
-        heapq.heapify(heap)
-        return [heapq.heappop(heap)[1] for _ in range(k)]
+        #print(count) # {'i':2, 'love': 2, 'leetcode': 1, 'coding': 1}
+        heap = [] 
+        for word, freq in count.items():
+            heappush(heap,(-freq,word))
+        # pop top k 
+        
+        res = []        
+        for i in range(k):
+            neg_val, key = heappop(heap)
+            res.append(key)
+        return res
+        
