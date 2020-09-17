8908320975c6f5fbec831eb5ed8fc34e29eb2878
kadhirash <kadhirash@gmail.com>
2020-09-17 23:56:46 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- problems/palindrome_number/solution.py
+++ problems/palindrome_number/solution.py
@@ -1,3 +1,33 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        # todo
+            # return true if number is palindrome
+        # ex:
+            # 121
+            # True 
+        # strat:
+            # reverse half the number 
+            # if x < 0: False
+            # pal = x
+                # while pal:
+                    # 121 % 10 --> 1 
+                    # 121 // 10 -> 12
+        
+        
+    
+        pal = x
+        pop = 0
+        
+        
+        if x < 0: return False
+        if x >= 0 and x < 10: return True
+        
+        
+        while pal:
+            pop = pop * 10 + pal % 10     # 1     ->  12   -> 121
+            
+            pal //= 10                    # 12    -> 1
+    
+        return pop == x
+        
+        
