658bb6daeb25b3e08b0b73ef4dceb60613d6f9a9
kadhirash <kadhirash@gmail.com>
2020-09-18 00:22:29 +0000

    Sync LeetCode submission - Word Search (python3)

--- problems/word_search/solution.py
+++ problems/word_search/solution.py
@@ -1,52 +1,42 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # clarifications:
-            # only english lowercase/uppercase
-            # can start anywhere
-        # todo:
-            # if word exists -> return true, else false
-        # example:  
-            # board =[ ['A','B','C','E'], ['S','F','C','S'], ['A','D','E','E']]
-                # ABCCED -> True
-                # SEE -> True
-                # ABCB -> False
-            
-        # strat:
-            # use a dfs 
-                # check the left, right, up, down sections
-    
-
-    
         
-        len_row, len_col = len(board), len(board[0])
-        for row in range(len_row):
-            for col in range(len_col):
+        row_len, col_len = len(board), len(board[0])
+        
+        for row in range(row_len):
+            for col in range(col_len):
                 if self.dfs(row,col,board,word,0):
                     return True
         return False
     
     
-        
+    
     def dfs(self,row,col,board,word,char):
         # validation check
-        len_row, len_col = len(board), len(board[0])
-        if char == len(word): 
+        row_len, col_len = len(board), len(board[0])
+        if char == len(word):
             return True
         
-        if row < 0 or col < 0 or row >= len_row or col >= len_col or board[row][col] != word[char]: 
-            return False
+        if row < 0 or col < 0 or row >= row_len or col >= col_len or board[row][col] != word[char]: return False
         
         
-        # save the state of the board
+        # save the board state
+        
         state = board[row][col]
         
-         # mark visited 
+        
+        # mark visited
+        
         board[row][col] = '#'
         
-        ans = self.dfs(row+1,col,board,word,char+1) or self.dfs(row-1,col,board,word,char+1) or self.dfs(row,col+1,board,word,char+1) or self.dfs(row,col-1,board,word,char+1)
+        # up,down,left,right
+        ans =   self.dfs(row-1,col,board,word,char + 1) or \
+                self.dfs(row+1,col,board,word,char + 1) or \
+                self.dfs(row,col+1,board,word,char + 1) or  \
+                self.dfs(row,col-1,board,word,char + 1)
         
-        # reset the state
+        # reset board state
+        
         board[row][col] = state
         
         return ans
-            
