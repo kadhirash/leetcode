455c5841e3e772a35ef01add9fffd16ebeffd4ff
kadhirash <kadhirash@gmail.com>
2020-09-19 04:25:56 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- problems/valid_palindrome/solution.py
+++ problems/valid_palindrome/solution.py
@@ -1,13 +1,26 @@
-from collections import deque
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #dq = deque()
-        # for chars in s:
-        #     if chars.isalnum():
-        #         dq.appendleft(''.join(chars.lower()))
-        dq = deque(''.join([chars for chars in s if chars.isalnum()]).lower())
-        while len(dq) > 1:
-            if dq.pop() != dq.popleft():
+        # 2 pointers to compare the left to the right side
+            # move the left side,right to the middle, and move right side, left to the middle
+            # if they are both of lenght 0, return True, else return False
+        
+        
+        if not s: 
+            return True
+        
+        left, right = 0, len(s)-1
+        
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            
+            if s[left].lower() != s[right].lower():
                 return False
+            else:
+                left += 1
+                right -= 1
         return True
-                
+            
