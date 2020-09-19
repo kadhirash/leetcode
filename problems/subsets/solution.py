6c5c4db0df0573d07c9cf6ca8a568ffed7630082
kadhirash <kadhirash@gmail.com>
2020-09-19 19:34:41 +0000

    Sync LeetCode submission - Subsets (python3)

--- problems/subsets/solution.py
+++ problems/subsets/solution.py
@@ -1,21 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        #to do:
-            # return all possible subsets 
-        # ex:
-            # [1,2,3]
-            # [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
         # strat:
-            # have an ans arr. holding []
-                # iterate through the digits in num
-                    # append the possible subsets(curr_int + digits) to the arr
-            # return arr
-            
-            
-        ans = [ [] ]
+            # use a stack to hold the empty set -> []
+            # append the subsets to the stack if exits in nums
         
-        for digs in nums:
-            for curr_dig in ans:
-                ans = ans + [curr_dig + [digs]]
+        
+        ans =[ []  ]
+        
+        for ints in nums:
+            for curr_int in ans:
+                ans = ans + [curr_int + [ints]]
         return ans
-            
