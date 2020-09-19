13bb84ee909a17d4f841b332a2450c952a252a26
kadhirash <kadhirash@gmail.com>
2020-09-19 04:00:11 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -1,21 +1,26 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        
+        # create two stack, one for the numbers and one for everything else
+        
         stack, num = [], []
-        stack.append([ [],1 ])
-        #print(stack)
+        # append empty stack and number 1 to the stack
+        stack.append([ [], 1])
         for chars in s:
+            # numbers -> both stacks
             if chars.isdigit():
                 num.append(chars)
+            # if left bracket
             elif chars == '[':
-                stack.append([ [], int("".join(num)) ])
-                #print(stack)
-                num = []
+                stack.append( [ [], int("".join(num)) ]  ) # append the empty stack as well as the previous value from numbers
+                num = [] # reset num stack
+            
+            # right bracket
             elif chars == ']':
-                st, k = stack.pop()
-                stack[-1][0].extend((st*k))
-                #print(stack)
-            else:
-                stack[-1][0].append(chars)
-                #print(stack)
-        return "".join(stack[0][0])
+                st, k = stack.pop() # pop off string + k (num) from stack since 2 pos. in stack
                 
+                stack[-1][0].extend((st*k)) # extend to the first pos. of tuple in stack the vals. of the popped off st*k 
+            
+            else:
+                stack[-1][0].append(chars) # if not a [] or number append only to first position of tuple in stack
+        return ''.join(stack[0][0]) # return the string 
