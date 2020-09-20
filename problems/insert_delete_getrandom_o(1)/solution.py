c29a195943c54ecaa710c21dcda19d697c1768f1
kadhirash <kadhirash@gmail.com>
2020-09-20 10:58:54 +0000

    Sync LeetCode submission - Insert Delete GetRandom O(1) (python3)

--- problems/insert_delete_getrandom_o(1)/solution.py
+++ problems/insert_delete_getrandom_o(1)/solution.py
@@ -4,6 +4,7 @@ class RandomizedSet:
         """
         Initialize your data structure here.
         """
+        # hashmap + list
         self.dict = {}
         self.list = []
 
@@ -13,30 +14,35 @@ class RandomizedSet:
         """
         if x in self.dict:
             return False
-        self.dict[x] = len(self.list)
-        self.list.append(x)
-        return True
+        else:
+            self.dict[x] = len(self.list)
+            self.list.append(x)
+            return True
+
     def remove(self, x: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
         if x in self.dict:
             last_elem = self.list[-1]
-            index = self.dict[x]
+            curr_index = self.dict[x] 
             
-            self.list[index] = last_elem
-            self.dict[last_elem] = index
+            self.list[curr_index] = last_elem
+            self.dict[last_elem] = curr_index
             
             self.list.pop()
             del self.dict[x]
+
             return True
         return False
+        
+
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        # return self.list[self.dict[x]]
-        return choice(self.list)
+        return random.choice(self.list)
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
