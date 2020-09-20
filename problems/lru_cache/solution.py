0e7dcf7fe3d852f849ba077703034e857f863b6b
kadhirash <kadhirash@gmail.com>
2020-09-20 10:30:52 +0000

    Sync LeetCode submission - LRU Cache (python3)

--- problems/lru_cache/solution.py
+++ problems/lru_cache/solution.py
@@ -1,27 +1,63 @@
+# DLL
+class Node:
+    def __init__(self,key,value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.dic = collections.OrderedDict()
         self.capacity = capacity
-
-    def get(self, key: int) -> int:
-        if key not in self.dic:
+        self.dict = {}
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def get(self, key: int) -> int: #refresh how new it was
+        if key in self.dict:
+            node = self.dict[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        else:
             return -1
-        value = self.dic[key]
-        del self.dic[key]
-        self.dic[key] = value
-        #self.dic.move_to_end(key)
-        return value
+        
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            del self.dic[key]
-        self.dic[key] = value
-        if len(self.dic) > self.capacity:
-            self.dic.popitem(last=False)
+        if key in self.dict:
+            self._remove(self.dict[key])
+            del self.dict[key]
+        elif len(self.dict) >= (self.capacity):
+            node = self.head.next
+            self._remove(node)
+            del self.dict[node.key]
+            
+        # add item to the MRU side of LRU cache
+        node = Node(key, value)
+        self.dict[key] = node   
+        self._add(node)
+    
+    def _remove(self,node): # remove node
+        prev = node.prev
+        prev.next = node.next
+        node.next.prev = prev
+    
+    
+    def _add(self, node): # add to tail for MRU side and pop from head for LRU side 
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+        
 
-
+    
+        
+    
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
