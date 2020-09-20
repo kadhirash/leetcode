9b9222f912fd82c51aa63fdb87aee2ea7e44bd0f
kadhirash <kadhirash@gmail.com>
2020-09-20 23:30:33 +0000

    Sync LeetCode submission - Word Search (python3)

--- problems/word_search/solution.py
+++ problems/word_search/solution.py
@@ -1,42 +1,46 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # strat:
+            # dfs (row,col, board,word,char)
+            
+            # mark visited cells
         
-        row_len, col_len = len(board), len(board[0])
         
-        for row in range(row_len):
-            for col in range(col_len):
+        
+        for row in range(len(board)):
+            for col in range(len(board[0])):
                 if self.dfs(row,col,board,word,0):
                     return True
         return False
     
     
     
-    def dfs(self,row,col,board,word,char):
-        # validation check
-        row_len, col_len = len(board), len(board[0])
+    
+    def dfs(self, row, col, board, word, char):
+        # validation check:
+            
         if char == len(word):
             return True
+        if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]) or board[row][col] != word[char]: return False
         
-        if row < 0 or col < 0 or row >= row_len or col >= col_len or board[row][col] != word[char]: return False
         
-        
-        # save the board state
-        
+        # save the state
         state = board[row][col]
         
-        
-        # mark visited
+        # mark the visited 
         
         board[row][col] = '#'
         
-        # up,down,left,right
-        ans =   self.dfs(row-1,col,board,word,char + 1) or \
-                self.dfs(row+1,col,board,word,char + 1) or \
-                self.dfs(row,col+1,board,word,char + 1) or  \
-                self.dfs(row,col-1,board,word,char + 1)
         
-        # reset board state
+        ans =   self.dfs(row+1,col,board,word, char + 1) or \
+                self.dfs(row-1,col,board,word, char + 1) or \
+                self.dfs(row,col-1,board,word, char + 1) or \
+                self.dfs(row,col+1,board,word, char + 1)
+        
+        
+        # reset to state
         
         board[row][col] = state
+        
         
         return ans
