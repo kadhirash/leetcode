f16ff619e6d6a64ffb70b7cc57cfe309ff005a34
kadhirash <kadhirash@gmail.com>
2020-09-21 01:33:39 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,42 +1,37 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-       # bfs
-    
-        if not grid or not grid[0]: return 0
-        
+        # dfs 
+            # mark visited
         
         count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.bfs(i,j,grid)
+        # edge case
+        if not grid: return 0
+        
+        # iterate through board and perform dfs
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '1': 
+                    self.dfs(row,col,grid)
                     count += 1
         return count
     
-    def bfs(self,i,j,grid):
-        if i <0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1': return 
+    
+    def dfs(self,row,col,grid):
+        # validation check
         
-        queue = collections.deque()
-        queue.append((i,j))
+        if row < 0 or col < 0 or row >=len(grid) or col >= len(grid[0]) or grid[row][col] != '1':
+            return
         
-        grid[i][j] = '#'
         
-        while queue:
-            (i,j) = queue.pop()
-            
-            if i-1 >= 0 and grid[i-1][j] == '1':
-                queue.append((i-1,j))
-                grid[i-1][j] = '0'
+        # mark visited
         
-            if i+1 < len(grid) and grid[i+1][j] == '1':
-                queue.append((i+1,j))
-                grid[i+1][j] = '0'
-                
-            if j-1 >= 0 and grid[i][j-1] == '1':
-                queue.append((i,j-1))
-                grid[i][j-1] = '0'
-                
-            if j+1 < len(grid[0]) and grid[i][j+1] == '1':
-                queue.append((i,j+1))
-                grid[i][j+1] = '0'
+        grid[row][col] = '#'
         
+        
+        self.dfs(row-1,col,grid)
+        self.dfs(row+1,col,grid)
+        self.dfs(row,col+1,grid)
+        self.dfs(row,col-1,grid)
+        
+        return grid
+    
