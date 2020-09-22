688d480c1bb6fb45aa7c0fb48820e605ac0a4ff2
kadhirash <kadhirash@gmail.com>
2020-09-22 19:58:17 +0000

    Sync LeetCode submission - Subsets (python3)

--- problems/subsets/solution.py
+++ problems/subsets/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # strat:
-            # use a stack to hold the empty set -> []
-            # append the subsets to the stack if exits in nums
+        # ans = [ [] ]
         
         
-        ans =[ []  ]
+        ans = [ [] ]
         
         for ints in nums:
             for curr_int in ans:
                 ans = ans + [curr_int + [ints]]
         return ans
+                    
