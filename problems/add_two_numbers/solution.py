57e520aaa6e610e8c4442cd45370ddeb8c89d6aa
kadhirash <kadhirash@gmail.com>
2020-09-22 16:12:29 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- problems/add_two_numbers/solution.py
+++ problems/add_two_numbers/solution.py
@@ -5,37 +5,28 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l3 = temp = ListNode(0)
+        # curr_node and an ans node, carry var
+        # iterate through the l2/l2 nodes
+            # add to carry
+        # curr.next -> carry % 10 
+        # curr = curr.next
+        # carry //10
+        # return ans.next 
+        
+        
+        ans = curr = ListNode(0)
         carry = 0
         
         while l1 or l2 or carry:
             if l1:
-                val1 = l1.val 
-            else:
-                val1 = 0
-            
-            if l2:
-                val2 = l2.val
-            else:
-                val2 = 0
-            
-            curr_sum = val1 + val2 + carry
-            
-            node = ListNode(curr_sum % 10)
-            carry = curr_sum // 10
-            
-            temp.next = node # poiints to curr node
-            temp = temp.next # update temp traversal
-            
-            if l1:
+                carry += l1.val
                 l1 = l1.next
-            else:
-                l1 = None
-                
             if l2:
+                carry += l2.val
                 l2 = l2.next
-            else:
-                l2 = None
                 
-        return l3.next
+            curr.next = ListNode(carry % 10)
+            curr = curr.next
+            carry //= 10
             
+        return ans.next
