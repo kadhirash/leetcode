0eb7e373a233f4cbd3f3c470c3bd0deaecde27e2
kadhirash <kadhirash@gmail.com>
2020-09-23 00:57:42 +0000

    Sync LeetCode submission - Two City Scheduling (python3)

--- problems/two_city_scheduling/solution.py
+++ problems/two_city_scheduling/solution.py
@@ -1,26 +1,20 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # strat:
-            # add up all the costs from city_a
-            # take the differences from second city - first city
-            # sort by the least negative values
-                # -350, -10 ...
-            # have a separate "refund" array to add to the costs
-                # ensure you have even amount from both cities as well as min. cost
-                
         
         
-        city_one = []
+        min_cost = 0
         differences = []
         n = len(costs) // 2
-        min_cost = 0
         for a,b in costs:
-            city_one.append(a)
+            min_cost += a
             differences.append(b-a)
-        min_cost = sum(city_one)
+            
         differences.sort()
+        
         
         for refund in range(n):
             min_cost += differences[refund]
             
         return min_cost
+    
+        
