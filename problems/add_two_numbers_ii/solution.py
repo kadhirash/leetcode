c6e2241282f18279a3b650dda5eff8ab9fded8fa
kadhirash <kadhirash@gmail.com>
2020-09-23 16:33:58 +0000

    Sync LeetCode submission - Add Two Numbers II (python3)

--- problems/add_two_numbers_ii/solution.py
+++ problems/add_two_numbers_ii/solution.py
@@ -5,10 +5,9 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        # 2 stacks
-            # l1/l2
-        # temp ListNode /carry var
-        
+        # 2 stacks 
+        # val % 10 
+        # carry 
         
         
         s1, s2 = [], []
@@ -21,6 +20,7 @@ class Solution:
                 s2.append(l2.val)
                 l2 = l2.next
                 
+        
         head = ListNode(0)
         carry = 0
         
@@ -29,17 +29,15 @@ class Solution:
                 carry += s1.pop()
             if s2:
                 carry += s2.pop()
-            
+                
             head.val = carry % 10
-            temp = ListNode (carry // 10) 
+            temp = ListNode(carry // 10)
             temp.next = head
             head = temp
-            carry //=10
+            carry //= 10
+            
+        
         if head.val == 0:
             return head.next
         else:
             return head
-            
-            
-        
-        
