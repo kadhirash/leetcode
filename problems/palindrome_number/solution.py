b10f56c0925a5f98d09cae2c5c6a2846bca10d4f
kadhirash <kadhirash@gmail.com>
2020-09-23 01:20:06 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- problems/palindrome_number/solution.py
+++ problems/palindrome_number/solution.py
@@ -1,30 +1,22 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # strat:
-            # reverse half the numbers -> O(logn)
-                # compare 1/2 with other 1/2
-                # pop modulo operator 10 -> pop off the last digit
-                # multiply the number * 10 
-                    # sum of mult + pop
-                # divide num // 10
-            # if negative, not 0, and ends with 0: return False
-        # ex:
-            # 12321 
+        # revert half the numbers
+        # 0 is a palindrome
+            # 1-9 not a palindrome
+            # can't end with 0
+            
         
         
-        
-        
-        if x < 0 or (x != 0 and x % 10 == 0):
+        if x < 0 or (x !=0 and x % 10 == 0):
             return False
         
-        
         revert_num = 0
-        
         while x > revert_num:
-            pop = x % 10
+            pop = x % 10 
             revert_num = revert_num * 10 + pop
             x = x // 10
             
         
-        return x == revert_num or x == revert_num//10
+        return revert_num == x or revert_num // 10 == x
             
+        
