82d498244429ab3235478f9d2d98d4bf08c1d796
kadhirash <kadhirash@gmail.com>
2020-09-24 20:37:47 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (python3)

--- problems/remove_all_adjacent_duplicates_in_string_ii/solution.py
+++ problems/remove_all_adjacent_duplicates_in_string_ii/solution.py
@@ -1,29 +1,15 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        # Clarifications:
-            # the answer is unique
-            # s only consists of lower case english letters
-        # To Do:
-            # return s w/o duplicates
-        # Example:
-            # deeedbbcccbdaa, k = 3     # remove e
-                # ddbbcccbdaa           # remove c
-                # ddbbbdaa              # remove b
-                # dddaa                 # remove d
-                # aa
-        
-        # Strategy:
-            # Use a stack to hold string
-                # Have a tuple in the stack (char, counter)
-            # iterate through the string
-                # when comparing the [-1][0] increment counter
-                    # if [-1][1] == k, then pop off k amount
-            # else append 
-            # make stack -> string and return
-                # .join in python and a for loop 
+        # dummy stack with char, count
+            # if count == k: remove
+            # else if same char: inc. count
+            # else append
+            
+        # join as a string
         
         
-        ans = [['#',0]]
+        ans = [['#', 0]]
+        
         for char in s:
             if ans[-1][0] == char:
                 ans[-1][1] += 1
@@ -31,8 +17,11 @@ class Solution:
                     ans.pop()
             else:
                 ans.append([char,1])
-        
+                
+                
         string = ''
         for i in range(len(ans)):
             string += ans[i][0] * ans[i][1]
+            
         return string
+        
