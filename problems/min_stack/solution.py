4343176752846f0b18fb3527b9e1c004fdc1c202
kadhirash <kadhirash@gmail.com>
2020-09-24 03:30:41 +0000

    Sync LeetCode submission - Min Stack (python3)

--- problems/min_stack/solution.py
+++ problems/min_stack/solution.py
@@ -5,17 +5,17 @@ class MinStack:
         initialize your data structure here.
         """
         self.stack = []
-        self.min_stack = []
+        self.min_stack = [] # val, count
 
     def push(self, x: int) -> None:
         self.stack.append(x)
-        if not self.min_stack or self.min_stack[-1][0] > x:
-            self.min_stack.append([x,1])
+        if not self.min_stack or x < self.min_stack[-1][0]:
+            self.min_stack.append([x,1]) 
         elif self.min_stack[-1][0] == x:
             self.min_stack[-1][1] += 1
 
     def pop(self) -> None:
-        if self.min_stack[-1][0] == self.stack[-1]: 
+        if self.min_stack[-1][0] == self.stack[-1]:
             self.min_stack[-1][1] -= 1
         if self.min_stack[-1][1] == 0:
             self.min_stack.pop()
