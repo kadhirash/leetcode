c714ec5d4ef19ca0e976cc0d600e6dc61ab09610
kadhirash <kadhirash@gmail.com>
2020-09-25 08:35:50 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- problems/valid_anagram/solution.py
+++ problems/valid_anagram/solution.py
@@ -1,27 +1,18 @@
+import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # q's:
-            # same length
-            # True 
-        
-        # strat:
-            # check if exists + same size
-            # hashmap to keep track of values in s and t
-                # iterate through s 
-                    # inc through s and dec. through t
-        
-        if not s and not t:
-            return True
         if len(s) != len(t):
             return False
         
         ans = collections.defaultdict(int)
         
-        for letters in range(len(s)):
-            ans[s[letters]] += 1
-            ans[t[letters]] -= 1
+        for letter in range(len(s)):
+            ans[s[letter]] += 1
+            ans[t[letter]] -= 1
             
-        for letters in ans:
-            if ans[letters] != 0:
+        
+        
+        for letter in ans:
+            if ans[letter] != 0:
                 return False
         return True
