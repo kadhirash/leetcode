05a33dd0f9640133ad7cee7039b33e4c1fabf609
kadhirash <kadhirash@gmail.com>
2020-09-25 02:49:13 +0000

    Sync LeetCode submission - Sort Characters By Frequency (python3)

--- problems/sort_characters_by_frequency/solution.py
+++ problems/sort_characters_by_frequency/solution.py
@@ -1,10 +1,20 @@
 class Solution:
     import heapq, collections
     def frequencySort(self, s: str) -> str:
-        # sort by decreasing freq. based on chars.
-        count = collections.Counter(s)
-        k = len(count)
-        ans = ''
-        for ch in heapq.nsmallest(len(count), count, key = lambda ch: (-count[ch], ch)):
-            ans += ch * count[ch]
-        return ans
+        # Count the number of occurrences of each unique character in the input string
+        counts = {}
+        for c in s:
+            counts[c] = counts.get(c, 0) + 1
+            
+        # Descending sort by number of occurrence (frequency), but ascending sort by character (lexicographic)
+        
+        sortedCounts = sorted((-n, c) for c, n in counts.items())
+                
+        # Concatenate character sequences
+        
+        ans = []
+        for n, c in sortedCounts:
+            ans.append(c*-n)
+        return ''.join(ans)
+
+
