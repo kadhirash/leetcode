e8f0b009d319aa19b62efc752dda5b0238a42209
kadhirash <kadhirash@gmail.com>
2020-09-25 02:18:19 +0000

    Sync LeetCode submission - Add Strings (python3)

--- problems/add_strings/solution.py
+++ problems/add_strings/solution.py
@@ -1,4 +1,31 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        # return sum of num1, num2, but they are strings
-        return str((int(num1) + int(num2)))
+        stack = []
+        
+        carry = 0
+        
+        nums1 = list(num1)
+        nums2 = list(num2)
+        
+        while len(nums1) > 0 or len(nums2) > 0  or carry:
+            if len(nums1) > 0:
+                x1 = ord(nums1.pop())- ord('0')
+            else:
+                x1 = 0
+            if nums2:
+                x2 = ord(nums2.pop())- ord('0')
+            else:
+                x2 = 0
+                
+            temp = x1+ x2 + carry
+            stack.append(temp % 10)
+            carry = temp // 10
+            
+        
+        ans = ''
+        for nums in stack:
+            ans += ''.join(str(nums))
+        return ans[::-1]
+        
+            
+                
