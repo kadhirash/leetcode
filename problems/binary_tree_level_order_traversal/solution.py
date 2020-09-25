679d98649421073a7e55e52e37fc14a53090609b
kadhirash <kadhirash@gmail.com>
2020-09-25 03:07:56 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (python3)

--- problems/binary_tree_level_order_traversal/solution.py
+++ problems/binary_tree_level_order_traversal/solution.py
@@ -4,28 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import collections 
-from collections import deque
+import collections
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return None
+        
+        queue = collections.deque()
+        queue.append(root)
         
         ans = []
-        queue = deque()
         
-        if not root:
-            return ans
-        
-        queue.append(root)
         while queue:
             size = len(queue)
             root_level = []
-            
             for i in range(size):
                 root = queue.popleft()
                 root_level.append(root.val)
+                
                 if root.left:
                     queue.append(root.left)
                 if root.right:
                     queue.append(root.right)
             ans.append(root_level)
         return ans
+                    
