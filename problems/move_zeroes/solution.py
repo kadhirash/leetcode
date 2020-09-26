b5463de3abefbaa4373f501f4b6b273e9e683fce
kadhirash <kadhirash@gmail.com>
2020-09-26 23:41:02 +0000

    Sync LeetCode submission - Move Zeroes (python3)

--- problems/move_zeroes/solution.py
+++ problems/move_zeroes/solution.py
@@ -1,11 +1,19 @@
 class Solution:
-    def moveZeroes(self, arr: List[int]) -> None:
+    def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero = 0 
-        for i in range(len(arr)):
-            if arr[i] != 0:
-                if i != zero:
-                    arr[i], arr[zero] = arr[zero], arr[i]
-                zero += 1
+        # zero = 0 
+        # for i in range(len(arr)):
+        #     if arr[i] != 0:
+        #         if i != zero:
+        #             arr[i], arr[zero] = arr[zero], arr[i]
+        #         zero += 1
+                
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[index] = nums[i]
+                index += 1
+        for i in range(index, len(nums)):
+            nums[i] = 0
