9483f855c050bf4e0f63f270009a749f1a8b7e41
kadhirash <kadhirash@gmail.com>
2020-09-26 18:25:57 +0000

    Sync LeetCode submission - Design Underground System (python3)

--- problems/design_underground_system/solution.py
+++ problems/design_underground_system/solution.py
@@ -1,18 +1,25 @@
 import collections
 class UndergroundSystem:
+
+    # 2 hashmaps
+        # check_in: id -> stationName, time
+        # journey: startStation,endStation -> total, count
+    # 
     def __init__(self):
-        self.check_in_data = {} # id -> (stationName,time)
-        self.journey_data = collections.defaultdict(lambda:[0,0]) # (startStation,endStation -> total, count)
+        self.check_in_data = {} # dictionary/hashmap
+        self.journey_data = collections.defaultdict(lambda:[0,0]) 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.check_in_data[id] = [stationName,t]
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         start_station, start_time = self.check_in_data.pop(id)
-        self.journey_data[(start_station,stationName)] [0] += (t-start_time)
-        self.journey_data[(start_station,stationName)] [1] += 1
         
+        self.journey_data[(start_station, stationName)] [0] += (t-start_time)
+        self.journey_data[(start_station, stationName)] [1] += 1
+
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        total_time, total_trips = self.journey_data[startStation,endStation]
+        total_time, total_trips = self.journey_data[(startStation,endStation)]
+        
         return total_time/total_trips
 
 
