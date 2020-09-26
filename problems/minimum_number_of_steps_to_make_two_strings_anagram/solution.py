5c68f2cb86cd7fbac201c92af4e31208fc6319ac
kadhirash <kadhirash@gmail.com>
2020-09-26 07:38:08 +0000

    Sync LeetCode submission - Minimum Number of Steps to Make Two Strings Anagram (python3)

---
+++ problems/minimum_number_of_steps_to_make_two_strings_anagram/solution.py
@@ -0,0 +1,11 @@
+import collections
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        count = collections.Counter(s)
+        res = 0
+        for c in t:
+            if count[c] > 0:
+                count[c] -= 1
+            else:
+                res += 1
+        return res
