418526045932ec9ff67c0d727dd3aea24f5e8fb5
kadhirash <kadhirash@gmail.com>
2020-09-26 08:57:31 +0000

    Sync LeetCode submission - Shuffle an Array (python3)

--- problems/shuffle_an_array/solution.py
+++ problems/shuffle_an_array/solution.py
@@ -1,21 +1,26 @@
+import random
 class Solution:
 
     def __init__(self, nums: List[int]):
         self.card_deck = nums
+        self.og = list(nums)
 
     def reset(self) -> List[int]:
         """
         Resets the array to its original configuration and return it.
         """
+        self.card_deck = self.og
+        self.og = list(self.og)
         return self.card_deck
 
     def shuffle(self) -> List[int]:
         """
         Returns a random shuffling of the array.
         """
-        shuffled_deck = self.card_deck[:]
-        random.shuffle(shuffled_deck)
-        return shuffled_deck
+        for i in range(len(self.card_deck)):
+            swap_i = random.randrange(i, len(self.card_deck))
+            self.card_deck[i] , self.card_deck[swap_i] = self.card_deck[swap_i], self.card_deck[i]
+        return self.card_deck
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
