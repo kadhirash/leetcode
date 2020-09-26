bdbf336e07e5676c65246615ad0ccc2b8233c99a
kadhirash <kadhirash@gmail.com>
2020-09-26 05:26:38 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -1,26 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        stack, curr_str, curr_count = [], '', 0
         
-        # create two stack, one for the numbers and one for everything else
-        
-        stack, num = [], []
-        # append empty stack and number 1 to the stack
-        stack.append([ [], 1])
-        for chars in s:
-            # numbers -> both stacks
-            if chars.isdigit():
-                num.append(chars)
-            # if left bracket
-            elif chars == '[':
-                stack.append( [ [], int("".join(num)) ]  ) # append the empty stack as well as the previous value from numbers
-                num = [] # reset num stack
+        for c in s:
             
-            # right bracket
-            elif chars == ']':
-                st, k = stack.pop() # pop off string + k (num) from stack since 2 pos. in stack
+            if c == '[':
+                stack.append(curr_str)
+                stack.append(curr_count)
+                curr_str = ''
+                curr_count = 0
+            elif c == ']':
+                prev_count = stack.pop()
+                prev_str = stack.pop()
+                curr_str = prev_str + prev_count * curr_str
+            elif c.isdigit():
+                curr_count = 10 * curr_count + int(c)
+            else: # character
+                curr_str += c
                 
-                stack[-1][0].extend((st*k)) # extend to the first pos. of tuple in stack the vals. of the popped off st*k 
-            
-            else:
-                stack[-1][0].append(chars) # if not a [] or number append only to first position of tuple in stack
-        return ''.join(stack[0][0]) # return the string 
+        return curr_str
+                
