b791ea197d6b52423824f25eb8497a9d87913ca7
kadhirash <kadhirash@gmail.com>
2020-09-27 21:32:43 +0000

    Sync LeetCode submission - Two City Scheduling (python3)

--- problems/two_city_scheduling/solution.py
+++ problems/two_city_scheduling/solution.py
@@ -1,20 +1,26 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        # 10 + 30 + 400 + 30 -> total
+        # differences = b-a 
+            # sort
+        # n times 
+        # total + n differences
+        # min_cost
         
         
         min_cost = 0
         differences = []
-        n = len(costs) // 2
+        
+        N = len(costs) // 2
+        
         for a,b in costs:
             min_cost += a
             differences.append(b-a)
             
-        differences.sort()
+        differences.sort() # nlogn
         
+        for i in range(N):
+            min_cost += differences[i]
         
-        for refund in range(n):
-            min_cost += differences[refund]
-            
         return min_cost
-    
         
