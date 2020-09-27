ab9771f5be115d422fbbd7cda34eea50b1126620
kadhirash <kadhirash@gmail.com>
2020-09-27 05:18:55 +0000

    Sync LeetCode submission - Word Ladder (python3)

--- problems/word_ladder/solution.py
+++ problems/word_ladder/solution.py
@@ -1,35 +1,31 @@
 import collections
 import string
 class Solution:
-    # string1 = "ABBK"
-    # string2 = "DBCN"
-    # dictionary = {"DBPN", "ABPN", "ABKK", "ABPN", "DCCN", "ABPK" }
-    def ladderLength(self, string1: str, string2: str, dictionary: List[str]) -> int:
-        word_set = set(dictionary)
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # create word_set
+        # visited set
+        # queue -> beginWord, length
+        # letters
+        
+        
+        
+        word_set = set(wordList)
         visited = set()
-        queue = collections.deque([(string1,1)]) # string1, length
-        alpha = string.ascii_lowercase
+        
+        queue = collections.deque()
+        queue.append((beginWord,1))
         
         while queue:
-            word,length = queue.popleft()
+            word,shortest_len = queue.popleft()
             size = len(word)
             
-            if word == string2:
-                return length
+            if word == endWord:
+                return shortest_len
             
             for i in range(size):
-                
-                for char in alpha:
-                    
-                    new_word = word[:i] + char + word[i+1:]
-                    
+                for j in string.ascii_lowercase:
+                    new_word = word[:i] + j + word[i+1:]
                     if new_word in word_set and new_word not in visited:
-                        
-                        queue.append((new_word,length+1))
+                        queue.append((new_word, shortest_len + 1))
                         visited.add(new_word)
         return 0
-        
-        # O(n * 26* m) 
-            # n = words
-            # m = len(word)
-        # O(n)
