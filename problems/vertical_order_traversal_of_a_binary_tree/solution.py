8d8e3724c5430f5a66e997d1ef8c7629505ff4e3
kadhirash <kadhirash@gmail.com>
2020-09-27 21:17:19 +0000

    Sync LeetCode submission - Vertical Order Traversal of a Binary Tree (python3)

--- problems/vertical_order_traversal_of_a_binary_tree/solution.py
+++ problems/vertical_order_traversal_of_a_binary_tree/solution.py
@@ -6,31 +6,38 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
-        if not root: return None
+        if not root:
+            return []
         
-        col_dict = defaultdict(list)
-        queue = deque([(root,0)])
-    
+        col_dict = collections.defaultdict(list)
+        
+        queue = collections.deque()
+        queue.append((root,0))
+        
         min_col = max_col = 0
         
         while queue:
             size = len(queue)
-            temp_dict = defaultdict(list)
-            for _ in range(size):
-                node, col= queue.popleft()
-                temp_dict[col].append(node.val)
+            temp_dict = collections.defaultdict(list)
             
-                if node.left:
-                    queue.append((node.left, col - 1))
-                    min_col = min(min_col, col - 1)
+            for i in range(size):
+                root,col = queue.popleft()
                 
-                if node.right:
-                    queue.append((node.right, col + 1))
+                if root.left:
+                    queue.append((root.left,col-1))
+                    min_col = min(min_col, col-1)
+                    
+                if root.right:
+                    queue.append((root.right, col+ 1))
                     max_col = max(max_col, col + 1)
-                
+                    
+                temp_dict[col].append(root.val)
+            
             for i in temp_dict:
                 col_dict[i] += sorted(temp_dict[i])
+        
         ans = []
-        for i in range(min_col, max_col+1):
+        
+        for i in range(min_col, max_col + 1):
             ans.append(col_dict[i])
         return ans
