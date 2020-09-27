8a15c74b8e2976ab0d11a654ece8f2de0f4a9dc8
kadhirash <kadhirash@gmail.com>
2020-09-27 05:29:55 +0000

    Sync LeetCode submission - Unique Paths (python3)

--- problems/unique_paths/solution.py
+++ problems/unique_paths/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1] * n for _ in range(m)] # first col/row all 1's
-        # right or down == 1 
+        dp = [[1] * n for _ in range(m)]
         
         for row in range(1,m):
             for col in range(1,n):
-                dp[row][col] = dp[row-1][col] + dp[row][col-1] # above ,left
-                
+                dp[row][col] = dp[row-1][col] + dp[row][col-1]
         return dp[m-1][n-1]
-                
-        
