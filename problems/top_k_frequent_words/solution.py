dfb557d4d3ad15b3dcbc7e6d3f60a14a0c14bb32
kadhirash <kadhirash@gmail.com>
2020-09-28 03:15:07 +0000

    Sync LeetCode submission - Top K Frequent Words (python3)

--- problems/top_k_frequent_words/solution.py
+++ problems/top_k_frequent_words/solution.py
@@ -1,23 +1,19 @@
-import collections
-import heapq
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-#         For example, your object is something like this in tuple's format (key, value_1, value_2)
-
-# When you put the objects (i.e. tuples) into heap, it will take the first attribute in the object (in this case is key) to compare. If a tie happens, the heap will use the next attribute (i.e. value_1) and so on.
-
-
-        freq_words = collections.Counter(words)
-        
+        if not words or not k:
+            return None
         heap = []
         
-        for key , val in freq_words.items():
-            heapq.heappush(heap, (-val, key))
-            
-        res = []
-        for _ in range(k):
-            res.append(heapq.heappop(heap)[-1])
+        count = collections.Counter(words)
         
-        return res
-                
+        
+        for word, freq in count.items():
+            heap.append((-freq,word))
+        
+        heapq.heapify(heap)
+        
+        ans = []
+        for _ in range(k):
+            ans.append(heapq.heappop(heap)[1])
+        return ans
             
