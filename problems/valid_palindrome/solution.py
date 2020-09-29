566b4bca7c08f40a7106c89d6bb309006715676f
kadhirash <kadhirash@gmail.com>
2020-09-29 05:04:23 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- problems/valid_palindrome/solution.py
+++ problems/valid_palindrome/solution.py
@@ -1,13 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 2 pointers to compare the left to the right side
-            # move the left side,right to the middle, and move right side, left to the middle
-            # if they are both of lenght 0, return True, else return False
-        
-        
-        if not s: 
-            return True
-        
         left, right = 0, len(s)-1
         
         while left < right:
@@ -15,12 +7,16 @@ class Solution:
                 left += 1
             while left < right and not s[right].isalnum():
                 right -= 1
-            
+                
+                
             
             if s[left].lower() != s[right].lower():
                 return False
-            else:
-                left += 1
-                right -= 1
+            left += 1
+            right -= 1
         return True
+            
+            
+            
+            
             
