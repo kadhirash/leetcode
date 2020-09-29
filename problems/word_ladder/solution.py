b2973e34056b1e9965780983e43727e2f87eb672
kadhirash <kadhirash@gmail.com>
2020-09-29 01:13:20 +0000

    Sync LeetCode submission - Word Ladder (python3)

--- problems/word_ladder/solution.py
+++ problems/word_ladder/solution.py
@@ -2,30 +2,35 @@ import collections
 import string
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # create word_set
-        # visited set
-        # queue -> beginWord, length
-        # letters
-        
-        
-        
+        # ex:
+            # beginWord: 'hit'
+            # endWord: 'cog'
+            # wordList:  ["hot","dot","dog","lot","log","cog"] # 4 iterations, length = 5
+        # qa:
+            # endWord in wordList
+            # no duplicates in wordList
+            # return 0 if no formation
+            # all lowercase
+        # strat:
+            # BFS
+                # queue
+            
         word_set = set(wordList)
         visited = set()
         
         queue = collections.deque()
-        queue.append((beginWord,1))
+        queue.append((beginWord,1)) # beginWord, length
         
         while queue:
-            word,shortest_len = queue.popleft()
-            size = len(word)
+            word,length = queue.popleft()
             
             if word == endWord:
-                return shortest_len
+                return length
             
-            for i in range(size):
+            for i in range(len(word)):
                 for j in string.ascii_lowercase:
                     new_word = word[:i] + j + word[i+1:]
-                    if new_word in word_set and new_word not in visited:
-                        queue.append((new_word, shortest_len + 1))
+                    if new_word not in visited and new_word in word_set:
+                        queue.append((new_word, length + 1))
                         visited.add(new_word)
         return 0
