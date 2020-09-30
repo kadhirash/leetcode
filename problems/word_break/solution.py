7e9559758bd255a535bc18088b24dc7f1cb6990b
kadhirash <kadhirash@gmail.com>
2020-09-30 03:12:35 +0000

    Sync LeetCode submission - Word Break (python3)

--- problems/word_break/solution.py
+++ problems/word_break/solution.py
@@ -1,8 +1,12 @@
 import collections
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # bfs using queue 
-            # deque 
+        # q/a 
+            # return false if empty string
+            
+        # strat:
+            # BFS via queue
+            
         
         word_set = set(wordDict)
         visited = set()
@@ -11,14 +15,14 @@ class Solution:
         queue.append(s)
         
         while queue:
-            s_1 = queue.popleft()
+            new_str = queue.popleft()
             
-            for word in word_set:
-                if s_1.startswith(word):
-                    new_str = s_1[len(word):]
-                    if new_str == '':
+            for i in word_set:
+                if new_str.startswith(i):
+                    substr = new_str[len(i):]
+                    if substr == "":
                         return True
-                    elif new_str not in visited:
-                        queue.append(new_str)
-                        visited.add(new_str)
+                    elif substr not in visited:
+                        queue.append(substr)
+                        visited.add(substr)
         return False
