6e8b166d51f1f064b4d6b2bae82b2638f23b9067
kadhirash <kadhirash@gmail.com>
2020-09-30 05:18:28 +0000

    Sync LeetCode submission - Design Underground System (python3)

--- problems/design_underground_system/solution.py
+++ problems/design_underground_system/solution.py
@@ -1,27 +1,21 @@
-import collections
 class UndergroundSystem:
-    # 2 hashmaps
-        # id -> stationName, time
-        # startStation,endStation - > total_time, count
+
     def __init__(self):
-        self.check_in_data = {}
-        self.journey_data = collections.defaultdict(lambda: [0,0])
+        self.check_in_data = {} # (id -> (startStation, t))
         
-    def checkIn(self,id, stationName,t):
+        self.journey_data = collections.defaultdict(lambda:[0,0]) # (startStation, end Station -> total_time, count) 
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.check_in_data[id] = [stationName,t]
-        
-    def checkOut(self,id,stationName,t):
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
         start_station,start_time = self.check_in_data.pop(id)
-        
-        self.journey_data[(start_station,stationName)][0] += t-start_time # total_time
-        self.journey_data[(start_station,stationName)][1] += 1 # count
-        
-    def getAverageTime(self,startStation,endStation):
-        total_time, total_trips = self.journey_data[(startStation,endStation)]
-        
-        return total_time/total_trips
-    
-        
+        self.journey_data[(start_station,stationName)][0] += (t-start_time)
+        self.journey_data[(start_station,stationName)] [1] += 1
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        total_trips, total_count = self.journey_data[(startStation,endStation)]
+
+        return total_trips / total_count
 
 
 # Your UndergroundSystem object will be instantiated and called as such:
