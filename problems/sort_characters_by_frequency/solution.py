d81bda21e0e6a127f89695680b7cef685d7e03fd
kadhirash <kadhirash@gmail.com>
2020-09-30 03:54:11 +0000

    Sync LeetCode submission - Sort Characters By Frequency (python3)

--- problems/sort_characters_by_frequency/solution.py
+++ problems/sort_characters_by_frequency/solution.py
@@ -1,12 +1,9 @@
-import collections
-import heapq
 class Solution:
     def frequencySort(self, s: str) -> str:
-        
         count = collections.Counter(s)
         k = len(count)
         ans = ''
-        
-        for c in heapq.nsmallest(k, count, key = lambda c: (-count[c], c)):
-            ans += c * count[c]
+        for c in heapq.nsmallest(k,count, key = lambda c: (-count[c], c)):
+            ans += count[c] *c
+            
         return ans
