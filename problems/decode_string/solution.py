c480173db58d69b59232d900fc522cf95253efad
kadhirash <kadhirash@gmail.com>
2020-09-30 00:50:30 +0000

    Sync LeetCode submission - Decode String (python3)

--- problems/decode_string/solution.py
+++ problems/decode_string/solution.py
@@ -1,15 +1,26 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        # [ 
-        # ]
-        # digit
-        # character
-        
-        # init
+        # q/a:
+            # k == number
+            # 1 -9 or > ?
+                # can be greater
+        # strat:
+            # stack, curr_str, curr_count
+            # 1) '['
+                # curr_str, curr_count 
+                # reset
+            # 2) ']'
+                # pop count , pop str
+            # 3) digit
+                # 10 + int(c)
+                    # 2[a1[b]] --> 21[ab]
+            # 4 ) character
+                # add to string
+                
+                
         stack = []
         curr_str = ''
         curr_count = 0
-        
         
         for c in s:
             if c == '[':
@@ -20,10 +31,13 @@ class Solution:
             elif c == ']':
                 prev_count = stack.pop()
                 prev_str = stack.pop()
-                curr_str = prev_str + prev_count * curr_str
+                curr_str = prev_str + curr_str * prev_count
             elif c.isdigit():
                 curr_count = 10 * curr_count + int(c)
-            else:
+            else: # char
                 curr_str += c
-            
+        
         return curr_str
+            
+            
+            
