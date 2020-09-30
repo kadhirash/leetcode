db4569e38017125683b0fd595b7806112579e415
kadhirash <kadhirash@gmail.com>
2020-09-30 03:29:43 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (python3)

--- problems/remove_all_adjacent_duplicates_in_string_ii/solution.py
+++ problems/remove_all_adjacent_duplicates_in_string_ii/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        # stack[-1][0] -> char
-        # stack[-1][1] -> count 
         stack = []
-        stack.append(('#',0)) # dummy var, count
-        
+        stack.append(('#',0))
         for c in s:
             if stack[-1][0] == c:
                 stack[-1][1] += 1
@@ -12,9 +9,8 @@ class Solution:
                     stack.pop()
             else:
                 stack.append([c,1])
-        
+                
         ans = ''
-        
         for c in range(len(stack)):
-            ans += stack[c][0] * stack[c][1]
+            ans+= stack[c][0] * stack[c][1]
         return ''.join(ans)
