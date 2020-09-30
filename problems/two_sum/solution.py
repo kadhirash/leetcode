46531bf03eae42c202ff468aa64ee4b0ed39cd4b
kadhirash <kadhirash@gmail.com>
2020-09-30 02:52:30 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ans = {}
+        
         for i in range(len(nums)):
             comp = target - nums[i]
             if comp in ans:
                 return ans[comp],i
             else:
                 ans[nums[i]] = i
+                
