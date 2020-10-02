70f456e7d6f67f966ec5982600ddfe7bc9ec6127
kadhirash <kadhirash@gmail.com>
2020-10-02 08:14:06 +0000

    Sync LeetCode submission - Trapping Rain Water (python3)

--- problems/trapping_rain_water/solution.py
+++ problems/trapping_rain_water/solution.py
@@ -1,31 +1,22 @@
 class Solution:
-    # if right_max > left_max, water depends on left_max
-    # if left_max > right_max, water depends on right_max
-    
-            # dependant on the min of both maxes 
-            # max on right side --> left 
-            # max on left side -> right
     def trap(self, height: List[int]) -> int:
-        left, right = 0, len(height) -1             # 0 ... 12
+        if not height:
+            return 0
+        # find index max height
+        max_h = height.index(max(height))
         
-        max_left = max_right = 0
-        
-        ans = 0
-        
-        
-        while left < right: # check when left < right                       # 0 < 12
-            if height[left] < height[right]: # elev. on right >             # since right side >, check left
-                
-                if max_left <= height[left]: # value at elev > max_left      # 
-                    max_left = height[left]
+        # assume height[-1] is max height
+        def trapping_water_till_end(height):
+            partial_sum, highest_level_seen = 0, float('-inf')
+            
+            for h in height:
+                if h >= highest_level_seen:
+                    highest_level_seen = h
                 else:
-                    ans += max_left - height[left]                          #
-                left += 1
-            else: # elev on left > 
-                
-                if max_right <= height[right]:
-                    max_right = height[right]
-                else:
-                    ans += max_right - height[right]
-                right -= 1
-        return ans
+                    partial_sum += highest_level_seen - h
+            return partial_sum
+        
+        return trapping_water_till_end(height[:max_h]) +\
+                trapping_water_till_end(reversed(height[max_h + 1:]))
+                    
+        
